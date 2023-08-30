4449765803c8c929eb585f50a250b80f6c0485fe
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-08-29 23:10:29 -0500

    created dog, cat, and lizard pages

---
+++ code.js
@@ -0,0 +1,24 @@
+// Add at least 3 dynamic components to your web page. These can be based on user input, or other dynamic input data.
+
+// At least one dynamic component should be based on user input.
+
+// At least one dynamic component should use a conditional statement to determine the output.
+
+// Stretch Goals
+// Try out the confirm() function, in addition to prompt(). When’s the right time to use this?
+
+// If the user gives you a color, can you set a part of your page to be that color?
+
+let userChoice = prompt('What type of animal are you looking to adopt? Options are a dog, cat, or lizard.')
+
+let lowercaseChoice = userChoice.toLowerCase();
+
+if (lowercaseChoice === 'dog'){
+    window.open('dog.html');
+} else if (lowercaseChoice === 'cat') {
+    window.open('cat.html')
+} else if (lowercaseChoice === 'lizard') {
+    window.open('lizard.html')
+} else {
+    alert('those are your only choices');
+}
