8083ed33292af4cc3e22b47a6dd44acc8eb14223
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-08-31 09:58:05 -0500

    corrected errors in index.html

--- JS/code.js
+++ JS/code.js
@@ -12,16 +12,16 @@
 
 'use strict mode'
 
-let userChoice = prompt('What type of animal are you looking to adopt? Options are a dog, cat, or lizard.')
+// let userChoice = prompt('What type of animal are you looking to adopt? Options are a dog, cat, or lizard.')
 
-let lowercaseChoice = userChoice.toLowerCase();
+// let lowercaseChoice = userChoice.toLowerCase();
 
-if (lowercaseChoice === 'dog') {
-    window.open('https://emmajohnsonbarth.github.io/DeltaVPetAdoptionCenter/dogs.html');
-} else if (lowercaseChoice === 'cat') {
-    window.open('https://emmajohnsonbarth.github.io/DeltaVPetAdoptionCenter/cats.html')
-} else if (lowercaseChoice === 'lizard') {
-    window.open('https://emmajohnsonbarth.github.io/DeltaVPetAdoptionCenter/lizards.html')
-} else {
-    alert('sorry, those are your only choices');
-}
+// if (lowercaseChoice === 'dog') {
+//     window.open('https://emmajohnsonbarth.github.io/DeltaVPetAdoptionCenter/dogs.html');
+// } else if (lowercaseChoice === 'cat') {
+//     window.open('https://emmajohnsonbarth.github.io/DeltaVPetAdoptionCenter/cats.html')
+// } else if (lowercaseChoice === 'lizard') {
+//     window.open('https://emmajohnsonbarth.github.io/DeltaVPetAdoptionCenter/lizards.html')
+// } else {
+//     alert('sorry, those are your only choices');
+// }
