a0008b57136e18daa9bb6494fa7e8f714b1be176
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-08-31 09:30:34 -0500

    moved info in HTML sections

--- code.js
+++ code.js
@@ -9,11 +9,14 @@
 
 // If the user gives you a color, can you set a part of your page to be that color?
 
+
+'use strict mode'
+
 let userChoice = prompt('What type of animal are you looking to adopt? Options are a dog, cat, or lizard.')
 
 let lowercaseChoice = userChoice.toLowerCase();
 
-if (lowercaseChoice === 'dog'){
+if (lowercaseChoice === 'dog') {
     window.open('https://emmajohnsonbarth.github.io/DeltaVPetAdoptionCenter/dogs.html');
 } else if (lowercaseChoice === 'cat') {
     window.open('https://emmajohnsonbarth.github.io/DeltaVPetAdoptionCenter/cats.html')
